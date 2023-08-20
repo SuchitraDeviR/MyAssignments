4b39916872bbf7c7e7f3089fe74934a82b86f6e4
SuchitraDeviR <142719000+SuchitraDeviR@users.noreply.github.com>
2023-08-20 17:41:16 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,21 @@
+package week1.day2;
+
+public class Calculator {
+	private int addTwoNumbers(int num1,int num2)
+	{
+		return num1+num2;
+	}
+	
+	public float mul(float num1,int num2)
+	{
+		return num1*num2;
+	}
+	
+	public void sub()
+	{
+		int num1=10;
+		int num2=5;
+		System.out.println(num1-num2);
+	}
+	
+}
---
+++ MyCalculator.java
@@ -0,0 +1,13 @@
+package week1.day2;
+
+public class MyCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator cal=new Calculator();
+		System.out.println(cal.mul(10, 20));
+		cal.sub();
+
+	}
+
+}
